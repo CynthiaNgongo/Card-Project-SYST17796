5ff1030b93d6a9484103941f1d776fa0fd5061ff
Leonardo <l.stifano96@gmail.com>
2019-04-14 12:51:11 -0400

    Changed CPU,GamePlayer,Money and PLayer classes

---
+++ src/ca/sheridancollege/project/CPU.java
@@ -0,0 +1,31 @@
+/*
+ * Leonardo Stifano
+ * Student ID: 991 532 946
+ * SYST10199 - Web Programming
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author lstif
+ */
+public class CPU extends Player{
+
+    public CPU(String playerID){
+        this.playerID = playerID;
+    }
+    @Override
+    public void play() {
+    }
+
+    @Override
+    public void win() {
+        wins++;
+    }
+
+    @Override
+    public void lose() {
+        losses++;
+    }
+    
+}
--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -5,44 +5,40 @@
  */
 package ca.sheridancollege.project;
 
-/**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- * @author dancye, 2018
- */
-public abstract class Player 
-{
-    private String playerID; //the unique ID for this player
-    
-    /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name)
-    {
-        playerID= name;
-    }
-    
-    /**
-     * @return the playerID
-     */
-    public String getPlayerID() 
-    {
+public abstract class Player {
+
+    protected String playerID; //the unique ID for this player
+    protected int wins, losses;
+
+    public String getPlayerID() {
         return playerID;
     }
 
-    /**
-     * Ensure that the playerID is unique
-     * @param givenID the playerID to set
-     */
-    public void setPlayerID(String givenID) 
-    {
+    public void setPlayerID(String givenID) {
         playerID = givenID;
     }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
     
-    /**
-     * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
-     */
+
     public abstract void play();
-    
+
+    public abstract void win();
+
+    public abstract void lose();
+
 }
