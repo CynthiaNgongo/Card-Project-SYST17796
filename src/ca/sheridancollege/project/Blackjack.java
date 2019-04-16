18d1bd852b2fa64abe431878378d70f932c7c545
leostifano <46826784+leostifano@users.noreply.github.com>
2019-04-15 21:19:45 -0400

    Update Blackjack.java

--- src/ca/sheridancollege/project/Blackjack.java
+++ src/ca/sheridancollege/project/Blackjack.java
@@ -41,10 +41,13 @@ public class Blackjack extends Game {
         cont = true;
         //loop through until the player runs out of money or cashes in
         while (cont) {
+            //before starting round, check if the user has run out of money
             if (player.getMoney() <= 0) {
+                //if the user runs out of money, break them out of the loop
                 System.out.println("Sorry, you ran out of money\nGoodbye!");
                 break;
             }
+            //take user input
             System.out.print("1 for Deal, 2 for Cash in: ");
             int deal = in.nextInt();
             in = new Scanner(System.in);
@@ -89,8 +92,11 @@ public class Blackjack extends Game {
                     //comparing values
                 }
 
-            } else {
+            } 
+            //if the user selects 2(or any other value), cash them out and find out if they won
+            else {
                 System.out.println("You cashed in for $" + player.getMoney() + "\nYour wins: " + player.getWins());
+                //player wins if they end with at least $1000
                 declareWinner();
                 System.out.println("Thank you, Goodbye!");
                 break;
@@ -98,36 +104,40 @@ public class Blackjack extends Game {
 
         }
     }
-
+    //check if they player won
     @Override
     public void declareWinner() {
         if (player.getMoney() >= 1000) {
             System.out.println("You have cashed out with $1000 or over\nYou won");
         } else {
-            System.out.println("Sorry you lost. You didn't cash out with $1000");
+            System.out.println("Sorry you lost. You needed at least $1000");
         }
     }
-
+    //compares the values of the player and cpu cards
+    //must check that the values are equal to 21 or over, before comparing cpu and player values
+    //otherwise, extra validation would be needed
     public void checkValue() {
+        //check player first
+        //player wins if they have 21
         if (playerHand.getValue() == 21) {
             player.win();
             System.out.println("You got 21! You win. You now have: $" + player.getMoney());
             System.out.println("The dealer had: " + cpuHand.getHand());
             done = true;
-
+        //cpu wins if value is 21 and the player did not
         } else if (cpuHand.getValue() == 21) {
             player.lose();
             System.out.println("The dealer has gotten 21. You lost and have $" + player.getMoney());
             System.out.println("The dealer had: " + cpuHand.getHand());
             done = true;
-
+        //player loses if they go over 21
         } else if (playerHand.getValue() > 21) {
 
             player.lose();
             System.out.println("You have gone bust. You have $" + player.getMoney());
             System.out.println("The dealer had: " + cpuHand.getHand());
             done = true;
-
+        //cpu loses if the go over 21 and the player hasn't
         } else if (cpuHand.getValue() > 21) {
 
             player.win();
