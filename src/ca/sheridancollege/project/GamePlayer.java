b15cd9c2a75c4e54af2b1f540fcd465fdbbf7783
leostifano <46826784+leostifano@users.noreply.github.com>
2019-04-15 22:11:27 -0400

    removed unnecessary methods + names + comments

--- src/ca/sheridancollege/project/GamePlayer.java
+++ src/ca/sheridancollege/project/GamePlayer.java
@@ -1,19 +1,16 @@
 /*
  * Leonardo Stifano
- * Student ID: 991 532 946
- * SYST10199 - Web Programming
+ * Cynthia Ngongo
+ * Vinushan Wickramasingam
+ * Tsz-Kin Yeung
  */
 package ca.sheridancollege.project;
 
-
-/**
- *
- * @author lstif
- */
 public class GamePlayer extends Player {
-
+    //Money field
     private Money money;
-
+    
+    //GamePLayer must be made with these parameters
     public GamePlayer(String playerID, Money money) {
         this.playerID = playerID;
         this.money = money;
@@ -22,29 +19,22 @@ public class GamePlayer extends Player {
 
     public void play() {
     }
-
+    
+    //call this function when the GamePlayer wins
+    //uses the add function from the Money class and increments player wins
     public void win() {
         money.add();
         wins++;
     }
-
+    //call this function when the GamePlayer loses
+    //uses the remove function from the money class
     public void lose() {
         money.remove();
     }
-
+    
+    //give the value of the Money
     public int getMoney() {
         return money.getAmount();
     }
-    
-    public void addMoney(){
-        money.add();
-    }
-    
-    public void removeMoney(){
-        money.remove();
-    }
-    
-    
-    
     
 }
